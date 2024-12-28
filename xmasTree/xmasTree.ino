e00453674ec433cb11a23189549d0294cea66436
gcronin <gabriel.cronin@gmail.com>
2024-12-28 11:47:47 -0800

    Alternating Proof of Concept Restore

--- xmasTree/xmasTree.ino
+++ xmasTree/xmasTree.ino
@@ -1,5 +1,11 @@
 #include "lightObject.h"
-#include "treeObject.h"
+
+uint8_t buttonPin = 2;
+uint8_t soundSensePin = 3;
+uint8_t redLEDPin = 13; 
+
+long timeStamp;
+boolean step = 1;
 
 Light light00 = Light(0,0);
 Light light01 = Light(0,1);
@@ -20,11 +26,39 @@ Light *lights[] = {&light00, &light01, &light02, &light03, &light04, &light05, &
                     &light10, &light11, &light12, &light13, &light14, &light15, &light16
 };
 
-Tree tree = Tree(lights);
-
 void setup() {
+  pinMode(redLEDPin, OUTPUT);
+  digitalWrite(redLEDPin, LOW);
+  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(soundSensePin, INPUT);
+  timeStamp = millis();
+
 }
 
 void loop() {
-  tree.AllOn();
+  if(step == 1) {
+    for (size_t i = 0; i < sizeof lights / sizeof lights[0]; i++) {
+      if((lights[i]->_xCoor + lights[i]->_yCoor)%2== 0) {
+        lights[i]->TurnOn();
+        delay(1);
+        lights[i]->TurnOff();
+      }
+    }
+  }
+  else {
+    for (size_t i = 0; i < sizeof lights / sizeof lights[0]; i++) {
+      if((lights[i]->_xCoor + lights[i]->_yCoor)%2== 1) {
+        lights[i]->TurnOn();
+        delay(1);
+        lights[i]->TurnOff();
+      }
+    }
+  }
+
+
+  if(millis() - timeStamp > 1000) {
+    timeStamp = millis();
+    step = 1- step;
+  }
+
 }
